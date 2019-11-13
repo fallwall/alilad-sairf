64cc97201a872f83efe50a3ffa723844958cc855
XX <fallwall19@gmail.com>
2019-11-12 22:48:45 -0500

    header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,16 @@ import sunflower from './sunflower.svg'
 export default function Header() {
   return (
     <div className="header">
-      <div>
-        <img src={sunflower} />
+      <div className="nav">
+        <img src={sunflower} style={{height:"20px"}}/>
+        <div>ABOUT</div>
+        <div>WRITING</div>
+        <div>SPEAKING</div>
+        <div>PROJECTS</div>
       </div>
-
+      <div>
+        <a href="#">Dalila Sairf</a>
+      </div>
     </div>
   )
 }
