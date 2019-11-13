53b7d4f564076731839e48b2f83f0812adf22c69
XX <fallwall19@gmail.com>
2019-11-12 23:24:11 -0500

    css for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,15 @@ export default function Header() {
   return (
     <div className="header">
       <div className="nav">
-        <img src={sunflower} style={{height:"20px"}}/>
-        <div>ABOUT</div>
-        <div>WRITING</div>
-        <div>SPEAKING</div>
-        <div>PROJECTS</div>
+        <div className="logo">
+          <img src={sunflower} style={{ height: "20px" }} />
+        </div>
+        <div className="nav-nav">
+          <div className="nav-nav-nav">ABOUT</div>
+          <div className="nav-nav-nav">WRITING</div>
+          <div className="nav-nav-nav">SPEAKING</div>
+          <div className="nav-nav-nav">PROJECTS</div>
+        </div>
       </div>
       <div>
         <a href="#">Dalila Sairf</a>
