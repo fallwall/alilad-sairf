84986f2da6a32b38b42d733a38249b3a5e6311b1
XX <fallwall19@gmail.com>
2019-11-12 22:05:58 -0500

    skeleton

---
+++ src/components/Container.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Contact from './Contact';
+import Social from './Social';
+
+export default function Container() {
+  return (
+    <div>
+      <Head />
+      <Main />
+      <Contact />
+      <Social />
+
+    </div>
+  )
+}
