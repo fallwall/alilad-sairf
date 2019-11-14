3a19b0ef2ac457eef2bf00035bae35e702f040ea
XX <fallwall19@gmail.com>
2019-11-14 10:02:13 -0500

    social links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,6 +4,13 @@ export default function Social() {
   return (
     <div className="social">
       <p>Some social links</p>
+      <a href="https://www.linkedin.com/in/dalilafrias/">
+        <i class="big linkedin icon"></i>
+      </a>
+      <a href="https://www.instagram.com/_blacksunflowers_/">
+        <i class="big instagram icon"></i>
+      </a>
+
     </div>
   )
 }
